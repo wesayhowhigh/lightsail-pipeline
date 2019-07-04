e8c23465c08c449220061b78f6db5849db52fbdb
philipjump <philip@wesayhowhigh.com>
2019-07-04 11:32:30 +0100

    Configure composer-auth.json

--- lightsail-build.sh
+++ lightsail-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cp /home/runner/secret /home/runner/${SEMAPHORE_PROJECT_NAME}/.env
-mv /home/runner/composer-auth.json /home/runner/${SEMAPHORE_PROJECT_NAME}/composer/auth.json
-
+mkdir /home/runner/.composer
+cp /home/runner/composer-auth.json /home/runner/.composer/auth.json
 
 set -e
 
@@ -18,7 +18,7 @@ TAG=v-${SEMAPHORE_BUILD_NUMBER}-${BRANCH_NAME}
 
 cd $DIR
 
-docker run --rm -w /opt -v $DIR:/opt -v $DIR/composer:~/.composer/auth.json $PHP_IMG composer install --no-dev
+docker run --rm -w /opt -v $DIR:/opt -v /home/runner/.composer:/root/.composer $PHP_IMG composer install --no-dev
 docker run --rm -w /opt -v $DIR:/opt $NODE_IMG npm install --registry https://npm-proxy.fury.io/iQe2xgJjTKscoNsbBNit/jump/
 docker run --rm -w /opt -v $DIR:/opt $NODE_IMG npm run build
 
