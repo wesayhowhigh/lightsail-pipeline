cfd6c353a5979914e68e1703f0e977cb46be3f46
Benjamin Jackson <benjacksondev@outlook.com>
2019-07-16 12:41:08 +0100

    Update lightsail-build.sh

--- lightsail-build.sh
+++ lightsail-build.sh
@@ -3,11 +3,6 @@
 set -e
 
 cp /home/runner/secret /home/runner/${SEMAPHORE_PROJECT_NAME}/.env
-
-if [ ! -d  "/home/runner/.composer" ]; then
-    mkdir /home/runner/.composer
-fi
-
 cp /home/runner/composer-auth.json /home/runner/.composer/auth.json
 
 DIR=$PWD
