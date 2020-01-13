3c98b8b59b7c3bc1ec2476e6fd98a657b76adffc
Jonathan Brown <hello@jonathanbrown.dev>
2020-01-13 11:35:22 +0000

    Update lightsail-build-gitn.sh

--- lightsail-build-gitn.sh
+++ lightsail-build-gitn.sh
@@ -32,7 +32,6 @@ DOCKER_LOGIN_CMD=$(aws ecr get-login --no-include-email)
 ssh ubuntu@${ORIGIN_SERVER_IP} ${DOCKER_LOGIN_CMD}
 ssh ubuntu@${ORIGIN_SERVER_IP} docker pull ${IMAGE}
 
-curl -o ./docker-compose.prod.yml https://raw.githubusercontent.com/wesayhowhigh/lightsail-pipeline/master/docker-compose.prod.yml
 sed -i "s|IMAGE_GOES_HERE|${IMAGE}|g" docker-compose.prod.yml
 
 scp docker-compose.prod.yml ubuntu@${ORIGIN_SERVER_IP}:~/docker-compose.prod.yml
