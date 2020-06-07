fea724830f3d0a1f5803f812c0e2ac5be94f4988
Benjamin Jackson <ben@wesayhowhigh.com>
2020-06-07 09:56:56 +0100

    Update lightsail-build.sh

--- lightsail-build.sh
+++ lightsail-build.sh
@@ -6,6 +6,7 @@ mkdir -p /home/runner/.composer
 
 cp /home/runner/secret /home/runner/${SEMAPHORE_PROJECT_NAME}/.env
 cp /home/runner/composer-auth.json /home/runner/.composer/auth.json
+cp /home/runner/nginx.conf /home/runner/${SEMAPHORE_PROJECT_NAME}/nginx.conf
 
 DIR=$PWD
 PHP_IMG="wesayhowhigh/php-app"
