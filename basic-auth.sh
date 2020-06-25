946591a61dfedde6b4c76ce788786e1b405dfcf4
Benjamin Jackson <ben@wesayhowhigh.com>
2020-06-25 10:36:16 +0100

    Update basic-auth.sh

--- basic-auth.sh
+++ basic-auth.sh
@@ -7,7 +7,6 @@ mkdir -p /home/runner/.composer
 cp /home/runner/secret /home/runner/${SEMAPHORE_PROJECT_NAME}/.env
 cp /home/runner/composer-auth.json /home/runner/.composer/auth.json
 cp /home/runner/nginx.protected.conf /home/runner/${SEMAPHORE_PROJECT_NAME}/nginx.conf
-cp /home/runner/htpasswd /home/runner/htpasswd
 
 DIR=$PWD
 PHP_IMG="wesayhowhigh/php-app"
