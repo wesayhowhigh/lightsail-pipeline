23e7f8efe9e5ade0e5012095f434486f8b912d82
Christopher David Heanan <chrissy.h1987@gmail.com>
2020-12-03 10:44:35 +0000

    Update to benefits sh

--- lightsail-build-benefitseveryone.sh
+++ lightsail-build-benefitseveryone.sh
@@ -17,7 +17,7 @@ cd $DIR
 # Authenticate with Docker Hub
 echo $DOCKER_PASSWORD | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-docker run --rm -w /opt -v $DIR:/opt -v /home/runner/.composer:/root/.composer $PHP_IMG composer install --no-dev --ignore-platform-reqs
+docker run --rm -w /opt -v $DIR:/opt -v /home/runner/.composer:/root/.composer $PHP_IMG composer install --no-dev --no-scripts
 docker run --rm -w /opt -v $DIR:/opt $NODE_IMG npm install --registry https://npm-proxy.fury.io/iQe2xgJjTKscoNsbBNit/jump/
 docker run --rm -w /opt -v $DIR:/opt $NODE_IMG npm run build
 
@@ -35,5 +35,18 @@ ssh ubuntu@${ORIGIN_SERVER_IP} docker pull ${IMAGE}
 
 sed -i "s|IMAGE_GOES_HERE|${IMAGE}|g" docker-compose.prod.yml
 
+curl -o ./cron.sh https://raw.githubusercontent.com/wesayhowhigh/lightsail-pipeline/master/cron.sh
+
+scp cron.sh ubuntu@${ORIGIN_SERVER_IP}:~/cron.sh
+
 scp docker-compose.prod.yml ubuntu@${ORIGIN_SERVER_IP}:~/docker-compose.prod.yml
+
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml run app composer install --no-dev
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml run app php artisan view:clear
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml run app php artisan cache:clear
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml run app php artisan config:cache
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml run app php artisan migrate:status
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml run app php artisan migrate --force
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml run app mkdir -p /var/www/html/storage/app/public
+
 ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml up -d
