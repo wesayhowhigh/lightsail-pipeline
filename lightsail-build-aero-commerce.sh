a4a8f617bda4158bd577acf8a4af84efbd3a61a1
Philip Belcher <mrsmeeth@gmail.com>
2021-02-03 16:39:14 +0000

    Add laravel6 nginx to aero build

--- lightsail-build-aero-commerce.sh
+++ lightsail-build-aero-commerce.sh
@@ -8,6 +8,7 @@ mkdir -p /home/runner/.composer
 
 cp /home/runner/secret /home/runner/${SEMAPHORE_PROJECT_NAME}/.env
 cp /home/runner/composer-auth.json /home/runner/.composer/auth.json
+cp /home/runner/nginx.laravel6.conf /home/runner/${SEMAPHORE_PROJECT_NAME}/nginx.conf
 
 DIR=$PWD
 PHP_IMG="wesayhowhigh/site:standard73node"
