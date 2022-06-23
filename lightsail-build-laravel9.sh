08a7b909436d4f479dbf46109849285f6ec531d0
Philip Belcher <31352729+philipbelcher@users.noreply.github.com>
2022-06-23 15:45:20 +0100

    Update lightsail-build-laravel9.sh

--- lightsail-build-laravel9.sh
+++ lightsail-build-laravel9.sh
@@ -12,7 +12,7 @@ cp /home/runner/nginx.laravel9.conf /home/runner/${SEMAPHORE_PROJECT_NAME}/nginx
 
 DIR=$PWD
 PHP_IMG="wesayhowhigh/site:standard8"
-NODE_IMG="node:15"
+NODE_IMG="node:14"
 TAG=v-${SEMAPHORE_DEPLOY_NUMBER}-${SEMAPHORE_BUILD_NUMBER}
 
 cd $DIR
