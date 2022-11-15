27187dd13af48501ca8c146541d9b944de76823d
Philip Belcher <31352729+philipbelcher@users.noreply.github.com>
2022-11-15 14:10:38 +0000

    Update lightsail-build-pot-dock-exit-pass.sh

--- lightsail-build-pot-dock-exit-pass.sh
+++ lightsail-build-pot-dock-exit-pass.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+
+ssh-keyscan -t rsa ${ORIGIN_SERVER_IP} >> ~/.ssh/known_hosts
+
 ssh ubuntu@${ORIGIN_SERVER_IP} sudo chown -R 1001:1001 ./ftp/autostore/upload
 ssh ubuntu@${ORIGIN_SERVER_IP} sudo chown -R 1002:1002 ./ftp/mandata/upload
