a5298301804996a8a43d0bbe643361c12781ef70
Philip Belcher <31352729+philipbelcher@users.noreply.github.com>
2023-02-27 15:59:25 +0000

    FML

--- lightsail-build-laravel9.sh
+++ lightsail-build-laravel9.sh
@@ -57,4 +57,4 @@ scp cron.sh ubuntu@${ORIGIN_SERVER_IP}:~/cron.sh
 scp docker-compose.prod.yml ubuntu@${ORIGIN_SERVER_IP}:~/docker-compose.prod.yml
 ssh ubuntu@${ORIGIN_SERVER_IP} docker system prune --all --force
 ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml up -d
-ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml exec -T app php project:set ${OCTOBER_LICENCE_KEY}
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml exec -T app php artisan project:set ${OCTOBER_LICENCE_KEY}
