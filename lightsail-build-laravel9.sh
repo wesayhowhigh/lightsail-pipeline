7b3192ecce9d6fbf5343e04f12b1e7e63b9cb9e3
Philip Belcher <31352729+philipbelcher@users.noreply.github.com>
2023-03-03 14:37:10 +0000

    Add user to project:set command

--- lightsail-build-laravel9.sh
+++ lightsail-build-laravel9.sh
@@ -58,5 +58,4 @@ scp docker-compose.prod.yml ubuntu@${ORIGIN_SERVER_IP}:~/docker-compose.prod.yml
 ssh ubuntu@${ORIGIN_SERVER_IP} docker system prune --all --force
 ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml up -d
 ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml exec -T app php artisan view:clear
-ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml exec -T app php artisan project:set ${OCTOBER_LICENCE_KEY}
-ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml exec -T app php artisan cache:clear
+ssh ubuntu@${ORIGIN_SERVER_IP} docker-compose -f docker-compose.prod.yml exec -T --user nginx:nginx app php artisan project:set ${OCTOBER_LICENCE_KEY}
